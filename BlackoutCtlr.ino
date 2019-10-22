40998be8d7236dc7903cee551189cdfabd69daee
Deo Ubuntu Work <dar8900@gmail.com>
2019-10-22 12:03:06 +0200

    - Inserito file iniziale di progetto e file di descrizione

---
+++ BlackoutCtlr.ino
@@ -0,0 +1,19 @@
+//Programma per il controllo dei blackout
+
+#include "Sim800l.h"
+
+
+
+void setup()
+{
+	Serial.begin(9600);
+	
+	
+}
+
+
+void loop()
+{
+	
+	
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Progetto per il controllo di blackout
